fbac80656699885136f08bcf407b5c79631fb7b4
yapavug <pavelguliaev2018@mail.ru>
2025-01-05 13:35:03 +0300

    Рассмотрение основного функционала вектора

--- main.cpp
+++ main.cpp
@@ -6,7 +6,28 @@ using namespace std;
 
 int main()
 {
-
-
+	vector<int> myVector;
+	myVector.push_back(1); // Добавление эл-та в конец
+	myVector.push_back(22);
+	myVector.push_back(333);
+	myVector.pop_back(); // Удаление эл-та из конца
+	cout << myVector.size() << endl; // Количество эл-тов вектора
+	for (int i = 0; i < myVector.size(); i++) // Как можно вывести эл-ты вектора
+	{
+		cout << myVector[i] << endl;
+	}
+	//cout << myVector[10] << endl; // Способ обращения к эл-ту вектора
+	//cout << myVector.at(10) << endl; // Способ обращения к эл-ту вектора (лучше)
+	//try
+	//{
+	//	cout << myVector.at(10) << endl;//Такой способ обращения к эл-там вектора предотвращает выход за границы вектора
+	//}
+	//catch (const std::out_of_range& ex)
+	//{
+	//	cout << ex.what() << endl;
+	//}
+	cout << myVector.size() << endl;
+	myVector.clear(); // "Очистка вектора"
+	cout << myVector.size() << endl;
 	return 0;
 }
